bc78857ec873904b3ea40795ee9b4425e5dcf345
MiguelCid <miguel.cid.paz@gmail.com>
2020-04-23 03:40:32 +0200

    microservicios-cursos

---
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/controllers/CursoController.java
@@ -0,0 +1,95 @@
+package com.formacionbdi.microservicios.app.cursos.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.microservicios.app.cursos.models.entity.Curso;
+import com.formacionbdi.microservicios.app.cursos.services.CursoService;
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
+import com.formacionbdi.microservicios.commons.controllers.CommonController;
+import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
+
+@RestController
+public class CursoController extends CommonController<Curso, CursoService>{
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editar (@RequestBody Curso curso, @PathVariable Long id){
+	
+		Optional<Curso>o = this.service.findById(id);
+		if(!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso dbCurso = o.get();
+		dbCurso.setName(curso.getName());
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+	}
+
+	@PutMapping("/{id}/asignar-alumnos")
+	public ResponseEntity<?> asignarAlumnos(@RequestBody List<Alumno> alumnos, @PathVariable Long id){
+		Optional<Curso>o = this.service.findById(id);
+		if(!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso dbCurso = o.get();
+		
+		alumnos.forEach(a -> {
+			dbCurso.addAlumno(a);
+		});
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+	}
+	
+	@PutMapping("/{id}/eliminar-alumno")
+	public ResponseEntity<?> eliminarAlumno(@RequestBody Alumno alumno, @PathVariable Long id){
+		Optional<Curso> o = this.service.findById(id);
+		if(!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso dbCurso = o.get();
+		
+		dbCurso.removeAlumno(alumno);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+	}
+	
+	@GetMapping("/alumno/{id}")
+	public ResponseEntity<?> buscarAlumnoPorId(@PathVariable Long id){
+		Curso curso = service.findCursoByAlumnoId(id);
+		return ResponseEntity.ok(curso);
+	}
+	
+	@PutMapping("/{id}/asignar-examenes")
+	public ResponseEntity<?> asignarExamenes(@RequestBody List<Examen> examenes, @PathVariable Long id){
+		Optional<Curso>o = this.service.findById(id);
+		if(!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso dbCurso = o.get();
+		
+		examenes.forEach(e -> {
+			dbCurso.addExamen(e);
+		});
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+	}
+	
+	@PutMapping("/{id}/eliminar-examen")
+	public ResponseEntity<?> eliminarExamen(@RequestBody Examen examen, @PathVariable Long id){
+		Optional<Curso> o = this.service.findById(id);
+		if(!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso dbCurso = o.get();
+		
+		dbCurso.removeExamen(examen);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+	}
+	
+}
