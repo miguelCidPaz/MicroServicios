20dd3974f32220473bb5d2b6280426ae59aae07d
MiguelCid <miguel.cid.paz@gmail.com>
2020-05-02 02:59:54 +0200

    microservicio

--- microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/controllers/CursoController.java
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/controllers/CursoController.java
@@ -1,10 +1,17 @@
 package com.formacionbdi.microservicios.app.cursos.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.microservicios.app.cursos.models.entity.Curso;
+import com.formacionbdi.microservicios.app.cursos.models.entity.CursoAlumno;
 import com.formacionbdi.microservicios.app.cursos.services.CursoService;
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.commons.controllers.CommonController;
@@ -19,6 +27,72 @@ import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
 
 @RestController
 public class CursoController extends CommonController<Curso, CursoService>{
+	
+	@Value("{config.balanceador.test}")
+	private String balanceadorTest;
+	
+	@DeleteMapping("/eliminar-alumno/{id}")
+	public ResponseEntity<?> eliminarCursoAlumnoPorId(@PathVariable Long id){
+		service.eliminarCursoAlumnoPorId(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	@Override
+	public ResponseEntity<?> listar(){
+		List<Curso>cursos = ((List<Curso>) service.findAll()).stream().map(c->{
+			c.getCursoAlumnos().forEach(ca ->{
+				Alumno alumno = new Alumno();
+				alumno.setId(ca.getAlumnoId());
+				c.addAlumno(alumno);
+			});
+			return c;
+		}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(cursos);
+	}
+	
+	@GetMapping("/pagina")
+	@Override
+	public ResponseEntity<?> listar(Pageable pageable){
+		Page<Curso> cursos = service.findAll(pageable).map(curso -> {
+			curso.getCursoAlumnos().forEach(ca ->{
+				Alumno alumno = new Alumno();
+				alumno.setId(ca.getAlumnoId());
+				curso.addAlumno(alumno);
+			});
+			return curso;
+		});
+		return ResponseEntity.ok().body(cursos);
+	}
+	
+	@GetMapping("/{id}")
+	@Override
+	public ResponseEntity<?> ver(@PathVariable Long id){
+		Optional <Curso> o = service.findById(id);
+		if(o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso curso = o.get();
+		
+		if(curso.getCursoAlumnos().isEmpty() == false) {
+			List<Long> ids= curso.getCursoAlumnos().stream().map(ca ->ca.getAlumnoId())
+					.collect(Collectors.toList());
+			
+			List<Alumno> alumnos = (List<Alumno>) service.obtenerAlumnosPorCurso(ids);
+			
+			curso.setAlumnos(alumnos);
+		}
+		
+		return ResponseEntity.ok().body(curso);
+	}
+	
+	@GetMapping("/balanceador-test")
+	public ResponseEntity<?> balanceadorTest(){
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("balanceador", balanceadorTest);
+		response.put("cursos", service.findAll());
+		return ResponseEntity.ok(response);
+	}
 	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar (@RequestBody Curso curso, @PathVariable Long id){
@@ -41,7 +115,10 @@ public class CursoController extends CommonController<Curso, CursoService>{
 		Curso dbCurso = o.get();
 		
 		alumnos.forEach(a -> {
-			dbCurso.addAlumno(a);
+			CursoAlumno cursoAlumno = new CursoAlumno();
+			cursoAlumno.setAlumnoId(a.getId());
+			cursoAlumno.setCurso(dbCurso);
+			dbCurso.addCursoAlumno(cursoAlumno);
 		});
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
 	}
@@ -53,8 +130,9 @@ public class CursoController extends CommonController<Curso, CursoService>{
 			return ResponseEntity.notFound().build();
 		}
 		Curso dbCurso = o.get();
-		
-		dbCurso.removeAlumno(alumno);
+		CursoAlumno cursoAlumno = new CursoAlumno();
+		cursoAlumno.setAlumnoId(alumno.getId());
+		dbCurso.removeCursoAlumno(cursoAlumno);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
 	}
@@ -62,6 +140,20 @@ public class CursoController extends CommonController<Curso, CursoService>{
 	@GetMapping("/alumno/{id}")
 	public ResponseEntity<?> buscarAlumnoPorId(@PathVariable Long id){
 		Curso curso = service.findCursoByAlumnoId(id);
+		
+		if (curso != null) {
+			List<Long> examenesIds = (List<Long>) service.obtenerExamenesIdsConRespuestasAlumno(id);
+			if (examenesIds != null && examenesIds.size() > 0) {
+				List<Examen> examenes = curso.getExamenes().stream().map(examen -> {
+					if (examenesIds.contains(examen.getId())) {
+						examen.setRespondido(true);
+					}
+					return examen;
+				}).collect(Collectors.toList());
+				curso.setExamenes(examenes);
+			}
+		}
+		
 		return ResponseEntity.ok(curso);
 	}
 	
--- microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
 
@@ -36,10 +38,14 @@ public class Curso {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
 	
-	@OneToMany(fetch = FetchType.LAZY)
+	@JsonIgnoreProperties(value= {"curso"}, allowSetters=true)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "curso", cascade = CascadeType.ALL, orphanRemoval=true)
+	private List <CursoAlumno> cursoAlumnos;
+	
+	@Transient
 	private List <Alumno> alumnos;
 	
-	@ManyToMany(fetch = FetchType.LAZY)
+	@OneToMany(fetch = FetchType.LAZY)
 	private List <Examen> examenes;
 	
 	@PrePersist
@@ -50,6 +56,7 @@ public class Curso {
 	public Curso() {
 		this.alumnos = new ArrayList<>();
 		this.examenes = new ArrayList<>();
+		this.cursoAlumnos= new ArrayList<>();
 	}
 
 
@@ -107,6 +114,22 @@ public class Curso {
 	
 	public void removeExamen(Examen examen) {
 		this.examenes.remove(examen);
+	}
+
+	public List<CursoAlumno> getCursoAlumnos() {
+		return cursoAlumnos;
+	}
+
+	public void setCursoAlumnos(List<CursoAlumno> cursoAlumnos) {
+		this.cursoAlumnos = cursoAlumnos;
+	}
+	
+	public void addCursoAlumno(CursoAlumno cursoAlumno) {
+		this.cursoAlumnos.add(cursoAlumno);
+	}
+	
+	public void removeCursoAlumno(CursoAlumno cursoAlumno) {
+		this.cursoAlumnos.remove(cursoAlumno);
 	}
 	
 }
